6024d162ad5214113e45bd416fafec6528e0ce0c
Rodrigo Magalhaes <rmagalhaes85@gmail.com>
2011-09-22 20:16:36 -0300

    - Trabalhos iniciais

--- cppcore/core.cpp
+++ cppcore/core.cpp
@@ -1,7 +1,14 @@
 #include <string>
+
+#include <iostream>
+#include <list>
+#include <memory>
 #include "core.h"
 
-namespace hal {
+
+namespace smsdenuncia {
+
+namespace device {
 
 GpsManager::GpsManager() {
 	
@@ -11,8 +18,9 @@ GpsManager::~GpsManager() {
 
 }
 
-int GpsManager::PutString(const std::string value) {
-	_buffer(value);
+int GpsManager::PutString(const std::string& value) {
+	_buffer = std::string(value);
+	return 0;
 }
 
 std::string GpsManager::GetString(void) {
@@ -20,15 +28,45 @@ std::string GpsManager::GetString(void) {
 }
 
 std::string GpsManager::SendCommand(std::string command) {
-	
+	return std::string("");
 }
 
-} // hal
+DeviceEquipmentStatus GpsManager::GetStatus() const {
+	int gpsStatus;
+	std::cout << "GpsManager: inform the GPS status: ";
+	std::cin >> gpsStatus;
+	return static_cast<smsdenuncia::device::DeviceEquipmentStatus>(gpsStatus);
+}
+
+PersistenceManager::PersistenceManager() {
+
+}
+
+PersistenceManager::~PersistenceManager() {
+
+}
+
+const std::list<std::string>& PersistenceManager::QueryList(const std::string& listName) {
+	std::auto_ptr< std::list< std::string > > resultList(new std::list < std::string >);
+	resultList->push_back(std::string("Teste"));
+	return static_cast<const std::list< std::string >& >(*resultList.release());	
+}
+
+int PersistenceManager::PersistValue(const std::string& key, const std::string& value) {
+	return 0;
+}
+
+DeviceEquipmentStatus PersistenceManager::GetStatus() const {
+	return Available;
+}
+
+} // device
 
 namespace core {
 
 Core::Core() {
-
+	_gpsManager = nullptr;
+	_persistenceManager = nullptr;
 }
 
 Core::~Core() {
@@ -36,15 +74,49 @@ Core::~Core() {
 }
 
 void Core::Initialize(void) {
-
+	std::cout << "Core: initializing..." << std::endl;
+	
+	std::cout << "Core: checking GPS device status..." << std::endl;
+	int gpsStatus = this->_gpsManager->GetStatus();
+	
+	{
+		using namespace smsdenuncia::device;
+		
+		if (gpsStatus == Available) std::cout << "Core: Gps Available" << std::endl;
+		else if (gpsStatus == NotAvailable) std::cout << "Core: Gps is not Available" << std::endl;
+		else std::cout << "Core: Unknown Gps status. Assuming Not Available" << std::endl;
+	}
 }
 
 const std::string Core::GetCurrentView(void) {
-
+	return static_cast<const std::string>(std::string(""));
 }
 
-const int Core::SetGpsManager(const hal::GpsManager gpsManager) {
+int Core::SetGpsManager(const device::GpsManager& gpsManager) {
+	_gpsManager = &gpsManager;
+	return 0;
+}
 
+int Core::SetPersistenceManager(const device::PersistenceManager& persistenceManager) {
+	_persistenceManager = &persistenceManager;
+	return 0;
 }
 
 } // core
+
+} // smsdenuncia
+
+
+int main (void) {
+	std::cout << "Inicializando core..." << std::endl;
+	
+	smsdenuncia::core::Core myCore;
+	smsdenuncia::device::GpsManager gpsManager;
+	
+	myCore.SetGpsManager(gpsManager);
+	
+	myCore.Initialize();
+	
+	std::cout << "Encerrando..." << std::endl;
+	return 0;
+}
--- cppcore/core.h
+++ cppcore/core.h
@@ -2,35 +2,52 @@
 #define CORE_H
 
 #include <string>
+#include <list>
 
 namespace smsdenuncia {
 
-namespace hal {
+namespace device {
 
-class HardwareManager {
-private:
-	HardwareManager& operator=(const HardwareManager& r) {}
-	HardwareManager(const HardwareManager& r) {}
-public:
-	virtual HardwareManager(void) = 0;
-	virtual ~HardwareManager(void) = 0;
-	virtual int PutString(const std::string value) = 0;
-	virtual std::string GetString(void) = 0;
-	virtual std::string SendCommand(std::string command) = 0;
+enum DeviceEquipmentStatus {
+	Available = 0,
+	NotAvailable = 1
 };
 
-class GpsManager : public HardwareManager {
+class DeviceEquipmentManager {
+private:
+	DeviceEquipmentManager& operator=(const DeviceEquipmentManager& r) {}
+	DeviceEquipmentManager(const DeviceEquipmentManager& r) {}
+public:
+	DeviceEquipmentManager() {};
+	virtual ~DeviceEquipmentManager() {};
+	virtual int PutString(const std::string& value) = 0;
+	virtual std::string GetString(void) = 0;
+	virtual std::string SendCommand(std::string command) = 0;
+	virtual DeviceEquipmentStatus GetStatus(void) const = 0;
+};
+
+class GpsManager : public DeviceEquipmentManager {
 private:
 	std::string _buffer;	
 public:
-	GpsManager(void);
-	~GpsManager(void);
-	int PutString(const std::string value);
+	GpsManager();
+	~GpsManager();
+	int PutString(const std::string& value);
 	std::string GetString(void);
 	std::string SendCommand(std::string command);
+	DeviceEquipmentStatus GetStatus(void) const;
 };
 
-} // hal
+class PersistenceManager : public DeviceEquipmentManager {
+public:
+	PersistenceManager();
+	~PersistenceManager();
+	const std::list<std::string>& QueryList(const std::string& listName);
+	int PersistValue(const std::string& key, const std::string& value);
+	DeviceEquipmentStatus GetStatus(void) const;
+};
+
+} // device
 
 namespace core {
 
@@ -39,12 +56,16 @@ public:
 	Core();
 	~Core();
 private:
+	const device::GpsManager * _gpsManager;
+	const device::PersistenceManager * _persistenceManager;
+
 	Core& operator=(const Core& r) {}
 	Core(const Core& r) {}
 public:
 	void Initialize(void);
 	const std::string GetCurrentView(void);
-	const int SetGpsManager(const hal::GpsManager gpsManager);
+	int SetGpsManager(const device::GpsManager& gpsManager);
+	int SetPersistenceManager(const device::PersistenceManager& persistenceManager);
 };
 
 } // core
